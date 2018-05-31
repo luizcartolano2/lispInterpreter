b636d2772f88923e44c85949c89aaa4b2d02d0df
Luiz Cartolano <duducartolano@gmail.com>
2018-05-31 20:56:34 -0300

    lisp interpreter working well

--- lisp.py
+++ lisp.py
@@ -78,7 +78,7 @@ def is_float(s):
 
 def is_string(s):
 	try:
-		if s[0] == "'" and s[-1] == "'"
+		if s[0] == "'" and s[-1] == "'":
 			return True
 	except ValueError:
 		return False
@@ -112,6 +112,93 @@ def do_parse(tokens):
 			ret.append(str(token[1:][0:-1]))
 		else:
 			ret.append(Symbol(token))
+
+# making the interpreter
+def eval(expr, environment):
+	if isinstance(expr, int):
+	    return expr
+	elif isinstance(expr, str):
+	    return expr
+	elif isinstance(expr, float):
+	    return expr
+	elif isinstance(expr, String):
+	    return expr.value
+	elif isinstance(expr, Symbol):
+	    if expr.value not in environment:
+	        fail("Couldn't find symbol {}".format(expr.value))
+	    return environment[expr.value]
+	elif isinstance(expr, list):
+		if isinstance(expr[0], Symbol):
+			if expr[0].value == 'lambda':
+				arg_names = expr[1]
+				code = expr[2]
+				return Lambda(arg_names, code)
+	elif expr[0].value == 'if':
+		condition = expr[1]
+		then = expr[2]
+		_else = None
+		if len(expr) == 4:
+		    _else = expr[3]
+
+		if eval(condition, environment) != False:
+		    return eval(then, environment)
+		elif _else is not None:
+		    return eval(_else, environment)
+	elif expr[0].value == 'define':
+		name = expr[1].value
+		value = eval(expr[2],environment)
+		environment[name] = value
+	elif expr[0].value == 'begin':
+		for ex in expr[1:]:
+			eval(ex,environment)
+	else:
+		fn = eval(expr[0],environment)
+		args = [eval(arg,environment) for arg in expr[1:]]
+		return apply(fn, args, environment)
+
+def apply(fn, args, environment):
+	if callable(fn):
+		return fn*args
+
+	if isinstance(fn, Lambda):
+		new_env = dict(environment)
+		if len(args) != len(fn.arguments):
+			fail("Mismatched number of arguments to lambda")
+		for i in range(len(fn.arguments)):
+			new_env[fn.arguments[i].value] = args[i]
+
+
+		return eval(fn.code, new_env)
+
+base_environment = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.div,
+    '>': operator.gt,
+    '>=': operator.ge,
+    '<': operator.lt,
+    '<=': operator.le,
+    '=': operator.eq,
+    '!=': operator.ne,
+    'nil': None,
+    'print': lambda x: sys.stdout.write(str(x) + '\n'),
+}
+
+# defining main function
+def main():
+	if len(sys.argv) != 2:
+		print "usage: python {} <file>".format(sys.argv[0])
+		exit(-1)
+
+	with open(sys.argv[1]) as fd:
+		contents = fd.read()
+		parsed = parse(tokenize(contents))
+		eval(parsed, base_environment)
+
+if __name__ == '__main__':
+	main()
+
 
 
 
